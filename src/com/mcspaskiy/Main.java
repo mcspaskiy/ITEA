ce7692161896eea982697939c2eb7e0bcf172fea
Mikhail Spaskiy <mcspaskiy@gmail.com>
2020-11-09 18:54:17 +0200

    Commit T2

--- src/com/mcspaskiy/Main.java
+++ src/com/mcspaskiy/Main.java
@@ -3,6 +3,6 @@ package com.mcspaskiy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("It will be the first commit in the third branch");
+        System.out.println("It will be the second commit in the third branch");
     }
 }
