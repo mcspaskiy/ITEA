7e40e59be8ae45c28e543d37a61e0fca5c8ae6bd
Mikhail Spaskiy <mcspaskiy@gmail.com>
2020-11-09 18:49:41 +0200

    Commit S3

--- src/com/mcspaskiy/Main.java
+++ src/com/mcspaskiy/Main.java
@@ -3,6 +3,6 @@ package com.mcspaskiy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("It's second commit in the second branch");
+        System.out.println("It's third commit in the second branch");
     }
 }
